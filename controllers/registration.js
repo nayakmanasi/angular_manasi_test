618e92692cfaea46a73d44ff27172268501d6635
nayakmanasi <manasinayak3@gmail.com>
2018-02-28 00:06:07 +0530

    Commit #3

--- controllers/registration.js
+++ controllers/registration.js
@@ -11,9 +11,6 @@ app.controller('registrationController', function($scope, $http){
  //      control.$setViewValue(undefined);
  //  	}	
 	};
-	$http.get("../phpqueries/selectusers.php").then(function (){
-			$scope.users = response.data.details;
-	});
 	$scope.insertRecord = function(){
 		//check for the existing email ID
 		// If email already exists then throw the error and dont insert
@@ -41,4 +38,21 @@ app.controller('registrationController', function($scope, $http){
 		}
 		console.log($scope.user);
 	};
+});
+app.controller('userController', function($scope, $http){
+	// Initial data
+	$scope.persons = [
+		{name:'Jani', email:'Norway@gmail.com', phone:'234234'},
+    {name:'Carl', email:'Sweden@gmail.com', phone:'56576721'},
+    {name:'Margareth', email:'England@gmail.com', phone:'0987887'},
+    {name:'Hege', email:'Norway@gmail.com', phone:'34234'},
+    {name:'Joe', email:'Denmark@gmail.com', phone:'234234'},
+    {name:'Gustav', email:'Sweden@gmail.com', phone:'567567'},
+    {name:'Birgit', email:'Denmark@gmail.com', phone:'234234'},
+    {name:'Mary', email:'England@gmail.com', phone:'3242342'},
+    {name:'Kai', email:'Norway@gmail.com', phone:'7675665'}
+	];
+	$http.get("../phpqueries/selectusers.php").then(function (){
+			$scope.users = response.data.details;
+	});
 });
